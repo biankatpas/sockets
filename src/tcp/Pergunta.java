9b2a1685b379439e9a2559b46a9fd405ed4026db
Bianka Passos <biankatpas@gmail.com>
2017-11-28 22:31:22 -0200

    criacao da classe pergunta p usar no quiz

---
+++ src/tcp/Pergunta.java
@@ -0,0 +1,41 @@
+package tcp;
+
+/**
+ *
+ * @author biankatpas
+ */
+
+public class Pergunta 
+{
+    private String pergunta;
+    private String resposta;
+
+    public Pergunta() {}
+
+    public Pergunta(String pergunta, String resposta) 
+    {
+        this.pergunta = pergunta;
+        this.resposta = resposta;
+    }
+
+    public String getPergunta() 
+    {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) 
+    {
+        this.pergunta = pergunta;
+    }
+
+    public String getResposta() 
+    {
+        return resposta;
+    }
+
+    public void setResposta(String resposta) 
+    {
+        this.resposta = resposta;
+    }
+        
+}
