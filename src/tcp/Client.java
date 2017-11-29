60385ff64c567a0d5b078a90659e1039315290e0
Bianka Passos <biankatpas@gmail.com>
2017-11-28 22:18:58 -0200

    cliente tcp usando scanner

--- src/tcp/Client.java
+++ src/tcp/Client.java
@@ -1,50 +1,62 @@
-/*
- * Client.java
- *
- * Created on 10 de Abril de 2005, 16:38
- */
-
 package tcp;
 
+import java.awt.HeadlessException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 /**
  *
- * @author Alysson
+ * @author biankatpas
  */
-public class Client {
-    public static void main(String[] args) {
-     
 
-        try{
-            String addrString = JOptionPane.showInputDialog("Digite o IP do Servidor: ");
+public class Client 
+{
+
+    public static void main(String[] args) 
+    {
+        String addrString = "";
+        int port;
+        Scanner s = new Scanner(System.in);
+
+        if (args.length < 2) 
+        {
+            addrString = "127.0.0.1";
+            port = 12345;
+        } 
+        else 
+        {
+            addrString = args[0];
+            port = Integer.parseInt(args[1]);
+        }
+        try 
+        {
             InetAddress addr = InetAddress.getByName(addrString);
-            String portString = JOptionPane.showInputDialog("Digite a Porta do Servidor: ");
-            int port = Integer.parseInt(portString);
-            String message = JOptionPane.showInputDialog("Digite uma mensagem para o Servidor: ");
-            
-            Socket socket = new Socket(addr,port);
 
-            DataOutputStream dataOutput = new DataOutputStream(socket.getOutputStream());
-            dataOutput.writeUTF(message);
-            
-            DataInputStream dataInput = new DataInputStream(socket.getInputStream());
-            String data = dataInput.readUTF();
-            socket.close();
+            //solicita a mensagem para enviar ao servidor
+            System.out.println("Digite uma mensagem para o servidor: ");
+            String message = s.nextLine();
 
-            if(data.equals(message)){
-                System.out.println("Echo: "+data+" - bem sucedido.");
-            }else{
-                System.out.println("Enviado: "+message);
-                System.out.println("Recebido: "+data);
+            String data;
+            try (Socket socket = new Socket(addr, port)) 
+            {
+                DataOutputStream dataOutput = new DataOutputStream(socket.getOutputStream());
+                dataOutput.writeUTF(message);
+                DataInputStream dataInput = new DataInputStream(socket.getInputStream());
+                data = dataInput.readUTF();
             }
-        }catch(Exception e){
-            System.err.println("An exception ocourred: "+e.getMessage());
-            e.printStackTrace();
+
+            if (data.equals(message)) 
+                System.out.println("Echo: " + data + " - bem sucedido.");
+            else 
+                System.out.println("Enviado: " + message + "\nRecebido: " + data);
+        } 
+        catch (HeadlessException | IOException e) 
+        {
+            System.err.println("An exception ocourred: " + e.getMessage());
             System.exit(-1);
         }
     }
