2bdf745bcd3f6ae02849f77b79590f82f1d27ab3
Vitor Passos <vitorpassos16@gmail.com>
2017-12-12 00:14:28 -0200

    testes funcionando com threads

--- src/exemplos/tcpThread/Server.java
+++ src/exemplos/tcpThread/Server.java
@@ -7,6 +7,7 @@ package exemplos.tcpThread;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.Scanner;
 
 /**
  *
@@ -15,16 +16,43 @@ import java.net.DatagramSocket;
 public class Server {
 
     public static void main(String[] args) {
-
+        int port = 12345;
+        DatagramSocket socket;
+        ThreadCliente tc;
+        Scanner s = new Scanner(System.in);
+        
         try {
+            socket = new DatagramSocket(port);
             DatagramSocket serverSocket = new DatagramSocket(1234);
             System.out.println("Servidor UDP escutando na porta " + serverSocket.getLocalPort());
 
             while (true) {
                 //Inicia thread do cliente
-                DatagramPacket dp = new DatagramPacket(new byte[1024],1024);
+                DatagramPacket dp = new DatagramPacket(new byte[1024], 1024);
                 serverSocket.receive(dp);
-                new ThreadCliente(dp).start();
+                tc = new ThreadCliente(dp);
+                tc.start();
+                tc.join();
+                
+                // Responder Cliente
+                
+                System.out.println("Selecione o que fazer com a pergunta do aluno: ");
+                System.out.println("1. solicitar esclarecimentos");
+                int opcao = s.nextInt();
+                String mensagem = "aaaa";
+                
+                switch (opcao) {
+                    case 1:
+                        mensagem = "Podes reformular sua pergunta";
+                        break;
+                    default:
+                        break;
+                }
+                
+                DatagramPacket solicita_cadastro = new DatagramPacket(mensagem.getBytes(), 0,
+                        mensagem.getBytes().length, dp.getAddress(), dp.getPort());
+                socket.send(solicita_cadastro);
+
             }
 
         } catch (Exception e) {
